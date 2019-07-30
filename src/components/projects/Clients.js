43d9b5ddf55b89141f4de9ba2421fee0fa0c6115
Prasad Madusanka <prasadmad96@gmail.com>
2019-07-30 14:28:16 +0530

    data obtainde

--- src/components/projects/Clients.js
+++ src/components/projects/Clients.js
@@ -1,6 +1,41 @@
-import React from "react";
+import React, { Component } from "react";
+import firebase from "firebase";
+import fbConfig from "../../config/fbConfig";
 
-const Client = () => {
-  return <h3>Usage</h3>;
-};
+class Client extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      person: []
+    };
+  }
+
+  componentDidMount() {
+    const app = firebase
+      .database()
+      .ref("Car_Parking")
+      .child("13 08 02 21");
+    app.on("value", snapshot => {
+      let values = snapshot.val();
+
+      let valueState = [];
+      for (let inn in values) {
+        console.log(inn);
+        valueState.push({
+          Acc_bal: values[inn].inn,
+          Name: values[inn].Name
+        });
+      }
+      this.setState({
+        person: valueState
+      });
+    });
+  }
+
+  render() {
+    return <h5>{this.state.person.Name}</h5>;
+  }
+}
+
 export default Client;
